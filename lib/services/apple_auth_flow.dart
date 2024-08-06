b1dfc21c5d9803996ebdb1b13d94de378b0edc37
ElectroChess <pranith.nag@outlook.com>
2024-08-05 22:17:17 -0700

    Implemented potential apple sign in solution

--- lib/services/apple_auth_flow.dart
+++ lib/services/apple_auth_flow.dart
@@ -15,14 +15,38 @@ class AppleAuthFlow {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<void> startAuthFlow(BuildContext context) async {
-    final credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-    );
-
-    print(credential);
+    try {
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      final credential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User user = userCredential.user!;
+      if (user == null) {
+        _handleFailedSignIn();
+        return;
+      }
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      if (!doc.exists) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'name': user.displayName,
+          'email': user.email,
+        });
+      }
+    } catch (error) {
+      print('Error during authentication: $error');
+      _handleFailedSignIn();
+    }
   }
 
   // ... rest of the class methods remain the sam
